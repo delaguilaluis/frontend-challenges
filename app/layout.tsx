173e8a159b61c3e83f15fd3f04b42cc1abd5a1f3
Luis Del Águila <luisdelaguila@outlook.com>
2023-06-21 19:17:16 +0200

    Add qr-code challenge

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <GlobalNav />
-        <main className="lg:pl-72">
+        <main className="lg:pl-72 bg-blue-100">
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
             {children}
           </div>
