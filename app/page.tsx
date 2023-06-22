66b97da85e261cd541b3bef4e95573aa883b1014
Luis Del Águila <luisdelaguila@outlook.com>
2023-06-22 11:26:54 +0200

    Improve landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { Container, Link, Typography } from "@mui/material";
+
 export default function Home() {
-  return <h1>Select one challenge from the menu</h1>;
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+      maxWidth="lg"
+    >
+      <Typography variant="h1" sx={{ fontSize: 24, textAlign: "center" }}>
+        These are solutions to Front-end Challenges from{" "}
+        <Link
+          underline="hover"
+          color="#666666"
+          href="https://frontendmentor.io"
+        >
+          FrontendMentor.io
+        </Link>{" "}
+        and{" "}
+        <Link underline="hover" color="#666666" href="https://frontloops.io">
+          Frontloops.io
+        </Link>
+        .<br></br> <br></br>
+        Please select one from the menu to get started.
+      </Typography>
+    </Container>
+  );
 }
