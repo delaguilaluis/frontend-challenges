a5fa260fc817eee45b673b0f7ad39a240760798b
Luis Del Águila <luisdelaguila@outlook.com>
2023-06-22 08:32:23 +0200

    Add segmented-control challenge

--- app/segmented-control/page.tsx
+++ app/segmented-control/page.tsx
@@ -1,3 +1,68 @@
+"use client";
+
+import {
+  Container,
+  FormControl,
+  MenuItem,
+  Select,
+  ToggleButton,
+  ToggleButtonGroup,
+  SelectChangeEvent,
+} from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useState } from "react";
+
 export default function Page() {
-  return <p>Segmented Control thing goes here</p>;
+  const [selection, setSelection] = useState("price");
+  // TODO: read this from the theme
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  function handleChange(event: React.MouseEvent<HTMLElement>, value: string) {
+    setSelection(value);
+  }
+
+  function handleSelectChange(event: SelectChangeEvent) {
+    setSelection(event.target.value);
+  }
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+      maxWidth="lg"
+    >
+      {isMobile && (
+        <FormControl fullWidth>
+          <Select
+            value={selection}
+            onChange={handleSelectChange}
+            sx={{ backgroundColor: "white" }}
+          >
+            <MenuItem value="price">Sort by price</MenuItem>
+            <MenuItem value="name">Sort by name</MenuItem>
+            <MenuItem value="relevance">Sort by relevance</MenuItem>
+          </Select>
+        </FormControl>
+      )}
+      {!isMobile && (
+        <ToggleButtonGroup
+          exclusive
+          color="primary"
+          value={selection}
+          onChange={handleChange}
+          aria-label="Platform"
+          sx={{ backgroundColor: "white" }}
+        >
+          <ToggleButton value="price">Sort by price</ToggleButton>
+          <ToggleButton value="name">Sort by name</ToggleButton>
+          <ToggleButton value="relevance">Sort by relevance</ToggleButton>
+        </ToggleButtonGroup>
+      )}
+    </Container>
+  );
 }
